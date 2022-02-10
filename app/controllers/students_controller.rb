6308aac222ed76f4cc1c264fc4e19aab05476532
Motti789 <mchillywilly@gmail.com>
2022-02-10 11:32:34 -0500

    created a index and a show page for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,10 @@
 class StudentsController < ApplicationController
+
+    def index
+      @students = Student.all
+    end
+
+    def show
+      @students = Student.find_by(params[:student_id]) 
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+teacher1 = Teacher.create(name: "Mrs. Yitty")
+teacher2 = Teacher.create(name: "Mr. Yoni")
+teacher3 = Teacher.create(name: "Mr. Avromi")
+
+student1 = Student.create(name: "Motti")
+student2 = Student.create(name: "Shmuli")
+student3 = Student.create(name: "Moish")
+
+course1 = Course.create(subject: "English Language", teacher_id: "1", student_id: "1")
+course2 = Course.create(subject: "Mathematics", teacher_id: "3", student_id: "2")
+course3 = Course.create(subject: "Science", teacher_id: "2", student_id: "3")
+course4 = Course.create(subject: "History", teacher_id: "1", student_id: "3")
+course5 = Course.create(subject: "Foreign Languages", teacher_id: "3", student_id: "1")
+
+
