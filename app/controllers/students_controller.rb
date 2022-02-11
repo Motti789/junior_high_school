c89bbc42aba12cd081046d6436560e6efe46ba6f
Motti789 <mchillywilly@gmail.com>
2022-02-10 20:22:28 -0500

    added a navbar

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,13 +5,13 @@ class StudentsController < ApplicationController
   end
 
   def create
-    if student = Student.create
-      session[:student_id] = student.id
-      redirect_to student_path(student)
+    @student = Student.new(student_params)
+     if @student.save
+     redirect_to root_path, notice: "Successfully created account"
     else
-      render 'new'
+       render :new
     end
-  end
+   end
   
   def index
     @students = Student.all
@@ -20,4 +20,11 @@ class StudentsController < ApplicationController
   def show
     @students = Student.find_by(id: params[:id]) 
   end
+
+  private
+
+def student_params
+  params.require(:student).permit(:name, :password)
+end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,7 @@ class Student < ApplicationRecord
   has_secure_password
   has_many :courses
   has_many :teachers, through: :courses
+  
+  validates_uniqueness_of :name
+  validates_presence_of :name
 end
